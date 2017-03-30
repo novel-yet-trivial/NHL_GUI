16140bc98784f31323ec5756e1cb89fcb66528c6
nyt <jonsspamaccout@gmail.com>
2017-03-30 13:12:38 -0700

    moved schedule to it's own frame

--- test.py
+++ test.py
@@ -29,35 +29,35 @@ except AttributeError:
 # teams in the order that they should be listed as 3-tuples: (city, name, image)
 TEAMS = [
     #~ ("Anaheim", "Ducks", "Anaheim_Ducks.gif"),
-    #~ ("Boston", " Bruins", "Boston_Bruins.gif"),
-    #~ ("Buffalo", " Sabres", "Buffalo_Sabres.gif"),
-    #~ ("Calgary", " Flames", "Calgary_Flames.gif"),
-    #~ ("Carolina", " Hurricanes", "Carolina_Hurricanes.gif"),
-    #~ ("Chicago", " Blackhawks", "Chicago_Blackhawks.gif"),
-    #~ ("Colorado", " Avalanche", "Colorado_Avalanche.gif"),
-    #~ ("Columbus", " Blue Jackets", "Columbus_Blue_Jackets.gif"),
-    #~ ("Dallas", " Stars", "Dallas_Stars.gif"),
-    #~ ("Detroit", " Red Wings", "Detroit_Red_Wings.gif"),
-    ("Edmonton", " Oilers", "Edmonton_Oilers.gif"),
-    ("Florida", " Panthers", "Florida_Panthers.gif"),
-    ("Los Angeles", " Kings", "Los_Angeles_Kings.gif"),
-    ("Minnesota", " Wild", "Minnesota_Wild.gif"),
-    ("Montreal", " Canadiens", "Montreal_Canadiens.gif"),
-    ("Nashville", " Predators", "Nashville_Predators.gif"),
-    ("New Jersey", " Devils", "New_Jersey_Devils.gif"),
-    ("New York", " Islanders", "New_York_Islanders.gif"),
-    ("New York", " Rangers", "New_York_Rangers.gif"),
-    ("Ottawa", " Senators", "Ottawa_Senators.gif"),
-    ("Philadelphia", " Flyers", "Philadelphia_Flyers.gif"),
-    ("Phoenix", " Coyotes", "Phoenix_Coyotes.gif"),
-    ("Pittsburgh", " Penguins", "Pittsburgh_Penguins.gif"),
-    ("San Jose", " Sharks", "San_Jose_Sharks.gif"),
-    ("St. Louis", " Blues", "St_Louis_Blues.gif"),
-    ("Tampa Bay", " Lightning", "Tampa_Bay_Lightning.gif"),
-    ("Toronto", " Maple Leafs", "Toronto_Maple_Leafs.gif"),
-    ("Vancouver", " Canucks", "Vancouver_Canucks.gif"),
-    ("Washington ", "Capitals", "Washington_Capitals.gif"),
-    ("Winnipeg ", "Jets", "Winnipeg_Jets.gif"),
+    #~ ("Boston", "Bruins", "Boston_Bruins.gif"),
+    #~ ("Buffalo", "Sabres", "Buffalo_Sabres.gif"),
+    #~ ("Calgary", "Flames", "Calgary_Flames.gif"),
+    #~ ("Carolina", "Hurricanes", "Carolina_Hurricanes.gif"),
+    #~ ("Chicago", "Blackhawks", "Chicago_Blackhawks.gif"),
+    #~ ("Colorado", "Avalanche", "Colorado_Avalanche.gif"),
+    #~ ("Columbus", "Blue Jackets", "Columbus_Blue_Jackets.gif"),
+    ("Dallas", "Stars", "Dallas_Stars.gif"),
+    #~ ("Detroit", "Red Wings", "Detroit_Red_Wings.gif"),
+    #~ ("Edmonton", "Oilers", "Edmonton_Oilers.gif"),
+    #~ ("Florida", "Panthers", "Florida_Panthers.gif"),
+    #~ ("Los Angeles", "Kings", "Los_Angeles_Kings.gif"),
+    #~ ("Minnesota", "Wild", "Minnesota_Wild.gif"),
+    #~ ("Montreal", "Canadiens", "Montreal_Canadiens.gif"),
+    #~ ("Nashville", "Predators", "Nashville_Predators.gif"),
+    #~ ("New Jersey", "Devils", "New_Jersey_Devils.gif"),
+    #~ ("New York", "Islanders", "New_York_Islanders.gif"),
+    #~ ("New York", "Rangers", "New_York_Rangers.gif"),
+    #~ ("Ottawa", "Senators", "Ottawa_Senators.gif"),
+    ("Philadelphia", "Flyers", "Philadelphia_Flyers.gif"),
+    ("Phoenix", "Coyotes", "Phoenix_Coyotes.gif"),
+    ("Pittsburgh", "Penguins", "Pittsburgh_Penguins.gif"),
+    ("San Jose", "Sharks", "San_Jose_Sharks.gif"),
+    ("St. Louis", "Blues", "St_Louis_Blues.gif"),
+    ("Tampa Bay", "Lightning", "Tampa_Bay_Lightning.gif"),
+    ("Toronto", "Maple Leafs", "Toronto_Maple_Leafs.gif"),
+    ("Vancouver", "Canucks", "Vancouver_Canucks.gif"),
+    ("Washington", "Capitals", "Washington_Capitals.gif"),
+    ("Winnipeg", "Jets", "Winnipeg_Jets.gif"),
 ]
 
 COLUMNS = 5
@@ -104,9 +104,6 @@ class StartPage(tk.Frame):
         self.Team_Name = StringVar()
         self.Team_Name.set('Unassigned')
 
-        self.Game_ID = StringVar()
-        self.Game_ID.set('Unassigned')
-
         teams = TeamGrid(self)
         teams.pack()
 
@@ -126,44 +123,95 @@ class StartPage(tk.Frame):
         else:
             self.master.show_frame(PageOne)
 
+
+class Schedule(ttk.Frame):
+    def __init__(self, master=None, highlight='', **kwargs):
+        print repr(highlight)
+        ttk.Frame.__init__(self, master, **kwargs)
+
+        self.error_lbl = tk.Label(self, fg="red")
+        self.error_lbl.place(relx=.5, rely=.5)
+
+        self.tree = ttk.Treeview(self)
+        self.tree.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+
+        vsb = ttk.Scrollbar(self)
+        vsb.pack(side=tk.RIGHT, fill=tk.Y, expand=True)
+        vsb.configure(command=self.tree.yview)
+        self.tree.configure(yscrollcommand=vsb.set)
+
+        self.tree.tag_configure('fav', background="blue")
+
+        col_headers = ("Away", "", "Home", "", "Clock", "Status")
+        col_widths = (100, 20, 100, 20, 150, 100)
+
+        columns = list(map(str, range(len(col_headers))))
+        self.tree.config(columns=columns)
+        self.tree.column('#0',width=0, minwidth=0)
+        for col, name, width in zip(columns, col_headers, col_widths):
+            self.tree.heading(col, text=name)
+            self.tree.column(col,width=width, minwidth=20)
+
+    def update(self, highlight=''):
+        #clear out previous values
+        self.tree.delete(*self.tree.get_children())
+
+        data = get_data()
+
+        if isinstance(data, str):
+            # an error occured
+            self.error_lbl.config(text=data)
+            self.error_lbl.tkraise()
+            return
+
+        self.tree.tkraise()
+
+        #Populate data in the treeview
+        for game_info in data['games']:
+            # Assign more meaningful names
+            game_clock = game_info['ts']
+            game_stage = game_info['tsc']
+            status = game_info['bs']
+
+            away_team_locale = game_info['atn']
+            away_team_name = game_info['atv'].title()
+            away_team_score = game_info['ats']
+            away_team_result = game_info['atc']
+
+            home_team_locale = game_info['htn']
+            home_team_name = game_info['htv'].title()
+            home_team_score = game_info['hts']
+            home_team_result = game_info['htc']
+
+            row = away_team_name, away_team_score, home_team_name, home_team_score, game_clock, status
+
+            tags='none'
+            if row[0] == highlight or row[2] == highlight:
+                tags = 'fav'
+                self.master.master.Game_ID.set(game_info['id'])
+            self.tree.insert('', 'end',values = row, tags=tags)
+
+
 class PageOne(tk.Frame):
     def __init__(self, master=None, **kwargs):
         tk.Frame.__init__(self, master, **kwargs)
 
-        Team_Name = master.frames[StartPage].Team_Name
-        Game_ID = master.frames[StartPage].Game_ID
-
-        label = tk.Label(self,text="Page One")
+        self.Game_ID = StringVar(self)
+        self.Game_ID.set('Unassigned')
 
         button1 = tk.Button(self, text="Back to Home",command=lambda: master.show_frame(StartPage))
         button1.grid(row=2,column=1)
 
         notebook = ttk.Notebook(self)
-        tab_schedule = ttk.Frame(notebook)
+        self.tab_schedule = Schedule(notebook)
         tab_live_game = ttk.Frame(notebook)
         tab_team_stats = ttk.Frame(notebook)
         tab_standings = ttk.Frame(notebook)
-        notebook.add(tab_schedule, text='Schedule')
+        notebook.add(self.tab_schedule, text='Schedule')
         notebook.add(tab_live_game, text='Live Game')
         notebook.add(tab_team_stats, text='Team Stats')
         notebook.add(tab_standings, text='Standings')
-        notebook.grid(row=1,column=1)
-
-        #********Schedule*********
-
-
-        vertscroll = tk.Scrollbar(tab_schedule, orient='vertical')
-        canvas_schedule = tk.Canvas(tab_schedule,yscrollcommand=vertscroll.set)
-        canvas_schedule.pack(side=LEFT,fill="both",expand=True)
-        vertscroll.pack(side=LEFT, fill=Y)
-        vertscroll.config(command = canvas_schedule.yview)
-
-        label_home_team = tk.Label(canvas_schedule, text="HOME")
-        label_home_team.grid(row=1,column=3)
-
-        label_away_team = tk.Label(canvas_schedule, text="AWAY")
-        label_away_team.grid(row=1,column=0)
-
+        notebook.grid(row=1,column=1, sticky='ns')
 
         #********Live Game*********
         lbl_home = tk.Label(tab_live_game, text="AWAY")
@@ -172,112 +220,52 @@ class PageOne(tk.Frame):
         lbl_away = tk.Label(tab_live_game, text="HOME")
         lbl_away.grid(row=1,column=4)
 
-        lbl_game_id = tk.Label(tab_live_game, text=Game_ID.get())
+        lbl_game_id = tk.Label(tab_live_game, textvariable=self.Game_ID)
         lbl_game_id.grid(row=1,column=6)
 
         #********Team Stats*********
 
         #********Standings*********
 
+    def tkraise(self):
+        '''called when this frame is opened'''
+        tk.Frame.tkraise(self)
+        self.after(10, self.tab_schedule.update, self.master.frames[StartPage].Team_Name.get())
 
-        i = datetime.datetime.now()  # date and time formatting http://www.cyberciti.biz/faq/howto-get-current-date-time-in-python/
-        api_url = 'http://live.nhle.com/GameData/RegularSeasonScoreboardv3.jsonp?loadScoreboard=jQuery110105207217424176633_1428694268811&_=1428694268812'
-        api_headers = {'Host': 'live.nhle.com', 'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.118 Safari/537.36', 'Referer': 'http://www.nhl.com/ice/scores.htm'}
-        # Format dates to match NHL API style:
+def get_data():
+    with open('nhl_data.json') as f:
+        data = json.load(f)
 
-        # Todays date
-        t = datetime.datetime.now()
-        todays_date = "" + t.strftime("%A") + " " + "%s/%s" % (t.month, t.day)
-        # Yesterdays date
-        y =y = t - datetime.timedelta(days=1)
-        yesterdays_date = "" + y.strftime("%A") + " " + "%s/%s" % (y.month, y.day)
-        while True:
-            try:
-                r = requests.get(api_url, headers=api_headers) #making sure there is a connection with the API
-            except (requests.ConnectionError): #Catch these errors
-                print ("Could not get response from NHL.com trying again...")
-                continue
-            except(requests.HTTPError):
-                print ("HTTP Error when loading url. Please restart program. ")
-                sys.exit(0)
-            except(requests.Timeout):
-                print ("The request took too long to process and timed out. Trying again... ")
-            except(socket.error):
-                print ("Could not get response from NHL.com trying again...")
-            except(requests.RequestException):
-                print ("Unknown error. Please restart the program. ")
-                sys.exit(0)
+    #~ api_url = 'http://live.nhle.com/GameData/RegularSeasonScoreboardv3.jsonp?loadScoreboard=jQuery110105207217424176633_1428694268811&_=1428694268812'
+    #~ api_headers = {'Host': 'live.nhle.com', 'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.118 Safari/537.36', 'Referer': 'http://www.nhl.com/ice/scores.htm'}
+#~
+    #~ try:
+        #~ r = requests.get(api_url, headers=api_headers) #making sure there is a connection with the API
+    #~ except (requests.ConnectionError): #Catch these errors
+        #~ return "Could not get response from NHL.com trying again..."
+    #~ except(requests.HTTPError):
+        #~ return "HTTP Error when loading url. Please restart program. "
+    #~ except(requests.Timeout):
+        #~ return "The request took too long to process and timed out. Trying again... "
+    #~ except(socket.error):
+        #~ return "Could not get response from NHL.com trying again..."
+    #~ except(requests.RequestException):
+        #~ return "Unknown error. Please restart the program. "
+    #~ # We get back JSON data with some JS around it, gotta remove the JS
+    #~ json_data = r.text
+#~
+    #~ # Remove the leading JS
+    #~ json_data = json_data.replace('loadScoreboard(', '')
+#~
+    #~ # Remove the trailing ')'
+    #~ json_data  = json_data[:-1]
+#~
+    #~ data = json.loads(json_data)
 
-            # We get back JSON data with some JS around it, gotta remove the JS
-            json_data = r.text
+    if 'games' not in data:
+        return "ERROR: no games found"
 
-            # Remove the leading JS
-            json_data = json_data.replace('loadScoreboard(', '')
-
-            # Remove the trailing ')'
-            json_data  = json_data[:-1]
-
-            data = json.loads(json_data)
-
-            #Used to keep track of the number of games played (number of rows on schedule)
-            num_games = 2 #start at 2 because row 1 is already used
-
-            for key in data:
-                if key == 'games':
-                    for game_info in data[key]:
-                        # Assign more meaningful names
-                        game_clock = game_info['ts']
-                        game_stage = game_info['tsc']
-                        status = game_info['bs']
-
-                        away_team_locale = game_info['atn']
-                        away_team_name = game_info['atv'].title()
-                        away_team_score = game_info['ats']
-                        away_team_result = game_info['atc']
-                        print(away_team_name)
-
-
-                        home_team_locale = game_info['htn']
-                        home_team_name = game_info['htv'].title()
-                        home_team_score = game_info['hts']
-                        home_team_result = game_info['htc']
-
-                        # Fix strange names / locales returned by NHL
-                        #away_team_locale = fix_locale(away_team_locale)
-                        #home_team_locale = fix_locale(home_team_locale)
-                        #away_team_name = fix_name(away_team_name)
-                        #home_team_name = fix_name(home_team_name)
-
-
-                        label_away = tk.Label(canvas_schedule, text=away_team_name,anchor='w')
-                        label_away.grid(row=num_games,column=0,pady=5,sticky=W)
-
-                        label_away_score = tk.Label(canvas_schedule, text=away_team_score)
-                        label_away_score.grid(row=num_games,column=1,pady=5)
-
-                        label_vs = tk.Label(canvas_schedule, text="vs")
-                        label_vs.grid(row=num_games,column=2,padx=5,pady=5)
-
-                        label_home = tk.Label(canvas_schedule, text=home_team_name,anchor='w')
-                        label_home.grid(row=num_games,column=3,pady=5,sticky=W)
-
-                        label_home_score = tk.Label(canvas_schedule, text=home_team_score)
-                        label_home_score.grid(row=num_games,column=4,pady=5)
-
-                        label_game_date = tk.Label(canvas_schedule, text=game_clock)
-                        label_game_date.grid(row=num_games,column=5,pady=5)
-
-                        label_game_status = tk.Label(canvas_schedule, text=status)
-                        label_game_status.grid(row=num_games,column=6,pady=5)
-
-                        num_games = num_games + 1 #increase the number of rows
-
-                        print (Team_Name.get())
-                        if str(Team_Name.get()) == str(away_team_name) or str(Team_Name.get()) == str(home_team_name):
-                            Game_ID.set(game_info['id'])
-
-            break
-            #End while true
+    return data
 
 def main():
     root = tk.Tk()
